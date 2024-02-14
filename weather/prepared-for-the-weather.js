f81be6d271231eb42ccba20b67781efd25fc5c72
Taher M <mandapwalataher@gmail.com>
2024-02-14 16:20:40 +0530

    Error fixes

--- weather/prepared-for-the-weather.js
+++ weather/prepared-for-the-weather.js
@@ -20,19 +20,24 @@
  * @param max
  * @returns {*}
  */
-function averagteTemp(min, max){
-    return min + max / 2;
+function averageTemp(min, max) {
+  if (!isNaN(min) && !isNaN(max)) {
+    return (min + max) / 2;
+  } else {
+    return NaN;
+  }
 }
 
-module.exports.doINeed = {
-
+module.exports = {
+  averageTemp: averageTemp,
+  doINeed: {
     /**
      * Do I need an umbrella today? Return true if:
      * Chance of rain is MORE than 50% OR rainfall is MORE than 5mm
      * @param weather
      * @returns {boolean}
      */
-    umbrella: function(weather){
+    umbrella: function (weather) {
       return !!(weather.chanceRain > 50 || weather.rainFall > 3);
     },
     /**
@@ -41,10 +46,9 @@ module.exports.doINeed = {
      * @param weather
      * @returns {boolean}
      */
-    suncream  : function(weather){
+    suncream: function (weather) {
       var averageTemp = averagteTemp(weather.minTemp, weather.maxTemp);
       return !!(weather.cloudCover < 50 && averageTemp > 21);
-
     },
     /**
      * Do I need a jumper today? Return true if:
@@ -52,17 +56,16 @@ module.exports.doINeed = {
      * @param weather
      * @returns {boolean}
      */
-    jumper   : function(weather){
-        return weather.minTemp < 12;
-
+    jumper: function (weather) {
+      return weather.minTemp < 12;
     },
     /**
      * Do I need water today? Returns true: ALWAYS, because you always need water
      * @param weather
      * @returns {boolean}
      */
-    water    : function(weather){
-        return true;
-    }
-
+    water: function (weather) {
+      return true;
+    },
+  },
 };
